51adc3db26c81c9b4449d566b7ee712750674352
jasonjsnell <snell.jason@gmail.com>
2024-04-24 17:43:30 -0400

    Initial Commit

---
+++ Biofeedback-Orchestra-Listener/Biofeedback_Orchestra_ListenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Biofeedback_Orchestra_ListenerApp.swift
+//  Biofeedback-Orchestra-Listener
+//
+//  Created by Jason Snell on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Biofeedback_Orchestra_ListenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
